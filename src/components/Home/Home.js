3399928374924f64ff8d90fa5ca8b6685695a872
Darina Beneva <beneva.darina@gmail.com>
2022-07-16 21:14:28 +0300

    Implementing home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import './Home.scss';
+
+function Home() {
+    return (
+      <div className="Home">
+        <img src="/food-4780114_960_720.jpg" alt="" />
+        <div class="text">
+            <p>Welcome to our very special place for sharing recipes!</p>
+            <a href="#">See Recipes</a>
+            <a href="#">Login</a>
+            <a href="#">Add New Recipe</a>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
