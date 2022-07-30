4276f9bd321012ef0ddb2d6636540034dd1f2d0c
Darina Beneva <beneva.darina@gmail.com>
2022-07-30 15:50:14 +0300

    Applying styling to footer page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Home />
+      <Footer></Footer>
     </div>
   );
 }
