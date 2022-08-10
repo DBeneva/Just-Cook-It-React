38179fc1512eb727986e2e1ee9a5da8eb4d36a0d
Darina Beneva <beneva.darina@gmail.com>
2022-08-10 17:27:53 +0300

    Implementing logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
+import { AuthProvider } from './contexts/AuthContext';
 import './App.css';
+
 import Home from './components/Home/Home';
 import Header from './components/common/Header/Header';
 import Footer from './components/common/Footer/Footer';
 import About from './components/About/About';
 import Recipes from './components/Recipe/Recipes/Recipes';
 import Recipe from './components/Recipe/Recipe/Recipe';
-import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/user/Login/Login';
 import Register from './components/user/Register/Register';
+import Logout from './components/user/Logout/Logout';
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
           <Route path="/recipes/:recipeId" element={<Recipe />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
 
         </Routes>
 
---
+++ client/src/components/user/Logout/Logout.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import * as authService from '../../../services/authService';
+import { useAuthContext } from '../../../contexts/AuthContext';
+
+const Logout = () => {
+    const { logout, user } = useAuthContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        authService.logout(user.accessToken)
+            .then(() => {
+                logout();
+                navigate('/');
+            });
+    }, []);
+
+    return null;
+};
+
+export default Logout;
