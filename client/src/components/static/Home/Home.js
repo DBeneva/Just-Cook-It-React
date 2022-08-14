02ad84157436080841e02d12312bf4ebcdd20c9e
Darina Beneva <beneva.darina@gmail.com>
2022-08-14 22:59:26 +0300

    Moving Home component to folder "static"

--- client/src/components/static/Home/Home.js
+++ client/src/components/static/Home/Home.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import { useAuthContext } from '../../contexts/AuthContext';
+import { useAuthContext } from '../../../contexts/AuthContext';
 import './Home.scss';
 
 function Home() {
