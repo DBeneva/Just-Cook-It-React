953dfdb9077875825267f920ca641585a109a11f
Darina Beneva <beneva.darina@gmail.com>
2022-08-14 23:58:16 +0300

    Cleaning up code in ConfirmationModal component

--- client/src/components/common/ConfirmationModal/ConfirmationModal.js
+++ client/src/components/common/ConfirmationModal/ConfirmationModal.js
@@ -7,18 +7,16 @@ function ConfirmationModal({
     toBeDeleted,
     name
 }) {
-    return (
-        show
-            ? (
-                <div className="ConfirmationModal">
-                    <p>Are you sure you want to delete your {toBeDeleted == 'recipe' ? 'recipe' : 'account,'}</p>
-                    <p>{toBeDeleted == 'recipe' ? 'for ' : ''}<span className="name">{name}</span>?</p>
-                    <button className="button" onClick={onClose}>Cancel</button>
-                    <button className="delete-btn button" onClick={onSave}> Delete < i className="fa fa-trash" ></i ></button >
-                </div>
-            )
-            : <></>
+    const modal = (
+        <div className="ConfirmationModal">
+            <p>Are you sure you want to delete your {toBeDeleted == 'recipe' ? 'recipe' : 'account,'}</p>
+            <p>{toBeDeleted == 'recipe' ? 'for ' : ''}<span className="name">{name}</span>?</p>
+            <button className="naked-btn button" onClick={onClose}>Cancel</button>
+            <button className="delete-btn button" onClick={onSave}> Delete < i className="fa fa-trash" ></i ></button >
+        </div>
     );
+
+    return show && modal;
 }
 
 export default ConfirmationModal;
