7754ada85db1810153c03c4e375733e896acd295
Darina Beneva <beneva.darina@gmail.com>
2022-08-14 20:43:42 +0300

    Cleaning up code in Footer component

--- client/src/components/common/Footer/Footer.js
+++ client/src/components/common/Footer/Footer.js
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
+
 import './Footer.scss';
 
 function Footer() {
     return (
         <div className="Footer">
             <p>Contacts:
-                <a href="#facebook-page"><i className="fab fa-facebook-square"></i></a>
-                <a href="#instagram"><i className="fab fa-instagram"></i></a>
-                <a href="mailto:contactus@justcookit.com"><i className="fas fa-envelope"></i></a>
+                <Link to="#facebook-page"><i className="fab fa-facebook-square"></i></Link>
+                <Link to="#instagram"><i className="fab fa-instagram"></i></Link>
+                <Link to="mailto:contactus@justcookit.com"><i className="fas fa-envelope"></i></Link>
             </p>
             <p><i className="fas fa-map-marker-alt"></i> Sofia 1000, 84 Vitosha Str</p>
-            <p><a href="/about">About Just Cook It</a></p>
+            <p><Link to="/about">About Just Cook It</Link></p>
         </div>
     );
 }
