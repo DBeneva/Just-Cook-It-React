0ed8b97ab268bf20d12a5c189e1a17a3b6483b42
Darina Beneva <beneva.darina@gmail.com>
2022-08-15 20:46:26 +0300

    Fixing loginc in getUsernameStatus Util

--- client/src/utils/getUsernameStatus.js
+++ client/src/utils/getUsernameStatus.js
@@ -1,6 +1,6 @@
 const getUsernameStatus = (value, form) => {
     if (value.length == 0) return 'invalid empty';
-    else if (form === 'register' && /[^a-zA-Z]/.test(value)) return 'invalid non-alphanumeric';
+    else if (form && /[^a-zA-Z]/.test(value)) return 'invalid non-alphanumeric';
     else if (value.length < 3) return 'invalid too-short';
     else return 'valid';
 };
