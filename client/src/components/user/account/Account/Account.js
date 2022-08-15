99b3861dd0e7b9e8bf9ab3de7c1726c8132360bc
Darina Beneva <beneva.darina@gmail.com>
2022-08-15 20:15:26 +0300

    Cleaning up code in Account component

--- client/src/components/user/account/Account/Account.js
+++ client/src/components/user/account/Account/Account.js
@@ -1,44 +1,42 @@
-import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../../../../contexts/AuthContext';
 
+import { useAuthContext } from '../../../../contexts/AuthContext';
 import './Account.scss';
 
 function Account() {
     const navigate = useNavigate();
     const { user } = useAuthContext();
-    const [error, setError] = useState(null);
 
     return (
         <div className="Account">
-    <div className="card-header">
-        <div className="image"><img src="/cooking-hat.png" alt="Cooking Hat" /></div>
-        <h2 className="title">{user.username}'s Account</h2>
-    </div>
 
-    {error && <p className="error">{error.message}</p>}
+            <div><img className="cooking-hat" src="/cooking-hat.png" alt="Cooking Hat" /></div>
+            <h2 className="title">{user.username}'s Account</h2>
 
-    <div className="field field-icon">
-        <p id="username"><span><i className="fas fa-user"></i></span> {user.username}</p>
-    </div>
+            <div>
+                <div className="section">
+                    <p id="username"><span><i className="fas fa-user"></i></span> {user.username}</p>
+                </div>
 
-    <div className="field field-icon">
-        <p><span><i className="fas fa-envelope"></i></span> {user.email}</p>
-    </div>
+                <div className="section">
+                    <p><span><i className="fas fa-envelope"></i></span> {user.email}</p>
+                </div>
 
-    <div className="field field-icon">
-        <p><span><img src="/instructions.png" /></span> <Link to="/recipes/my-recipes">My Recipes</Link></p>
-    </div>
+                <div className="section">
+                    <p><span><img className="fas" src="/instructions.png" /></span> <Link to="/recipes/my-recipes">My Recipes</Link></p>
+                </div>
 
-    <div className="field field-icon">
-        <p><span><i className="fas fa-heart"></i></span> <Link to="/recipes/my-favorites">My Favourite Recipes</Link></p>
-    </div>
+                <div className="section">
+                    <p><span><i className="fas fa-heart"></i></span> <Link to="/recipes/my-favorites">My Favourite Recipes</Link></p>
+                </div>
 
-    <div className="buttons">
-        <button className="button" onClick={() => navigate(-1)}>Go Back</button>
-        <Link className="button" to={`/users/${user._id}/edit`}>Edit Account</Link>
-    </div>
-</div>
+                <div className="buttons">
+                    <button className="button cancel-btn" onClick={() => navigate(-1)}>Go Back</button>
+                    <Link className="button" to={`/users/${user._id}/edit`}>Edit Account</Link>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
