7584121c73aa6b72f17aa517c4d1ea4dc2de1e37
Darina Beneva <beneva.darina@gmail.com>
2022-08-18 20:11:22 +0300

    Cleaning up getPasswordStatus code

--- client/src/utils/getPasswordStatus.js
+++ client/src/utils/getPasswordStatus.js
@@ -1,8 +1,8 @@
 const getPasswordStatus = (value, form) => {
     if (value.length == 0) return 'invalid empty';
     else if (value.length < 5) return 'invalid too-short';
-    else if (form === 'register' && /[а-яА-Я]/.test(value)) return 'invalid non-latin-letters';
-    else if (form === 'register' && !/[\!\?@\#\$%\^\&\*\(\)]/.test(value)) return 'invalid no-special-symbol';
+    else if (form && /[а-яА-Я]/.test(value)) return 'invalid non-latin-letters';
+    else if (form && !/[\!\?@\#\$%\^\&\*\(\)]/.test(value)) return 'invalid no-special-symbol';
     else return 'valid';
 };
 
