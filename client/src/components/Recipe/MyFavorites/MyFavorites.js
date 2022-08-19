11ecf2e27afec5c63be78a8a72d3687931c40eb1
Darina Beneva <beneva.darina@gmail.com>
2022-08-19 22:08:53 +0300

    Cleaning up code in MyFavorites and MyRecipes components

--- client/src/components/Recipe/MyFavorites/MyFavorites.js
+++ client/src/components/Recipe/MyFavorites/MyFavorites.js
@@ -2,67 +2,54 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
 import * as contentService from '../../../services/contentService';
-import { useAuthContext } from '../../../contexts/AuthContext';
-
-import './MyFavorites.scss';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import './MyFavorites.scss';
 
 function MyFavorites() {
-    const { user } = useAuthContext();
     const navigate = useNavigate();
     const [error, setError] = useState(null);
     const [myFavorites, setMyFavorites] = useState({});
 
     useEffect(() => {
-        contentService.loadMyFavorites(user)
+        contentService.loadMyFavorites()
             .then(recipes => setMyFavorites(recipes))
             .catch(err => {
-                console.log(`Error: ${err}`);
+                setError(err);
+                console.error(err);
             });
     }, []);
 
-    const buttons = user.username
-        ? (
-            <div>
-                <p>Have a look at all the recipes and like them for easy access!</p>
-                <Link className="button" to="/recipes">See Recipes</Link>
-                <button className="button" onClick={() => navigate(-1)}>Go Back</button>
-            </div>
-        )
-        : <Link className="button" to="/">Go back home</Link>;
+    const buttons = (
+        <div>
+            <p>Have a look at all the recipes and like them for easy access!</p>
+            <Link className="button" to="/recipes">See Recipes</Link>
+            <button className="button cancel-btn" onClick={() => navigate(-1)}>Go Back</button>
+        </div>
+    );
+
+    const myFavoriteRecipeCards = (
+        <div className="recipes">
+            <h2 className="title">My Favorites</h2>
+            {myFavorites.map(r => <RecipeCard key={r._id} recipe={r} />)}
+        </div>
+    );
+
+    const noRecipes = (
+        <div className="no-recipes">
+            <h2 className="title">Sorry, you haven't liked any recipes yet.</h2>
+            {buttons}
+        </div>
+    );
 
     return (
         <div className="MyFavorites">
+            {error && <p className="error">{error.message}</p>}
 
-            {/* <!-- <app-loading-recipes *ngIf="!myRecipes"></app-loading-recipes> --> */}
-            {
-                myFavorites && myFavorites.length > 0
-                    ?
-                    (
-                        <div className="recipes">
-                            <h2 className="title">My Favorites</h2>
-                            {myFavorites.map(r => <RecipeCard key={r._id} recipe={r} />)}
-                        </div>
-                    )
-                    : myFavorites && myFavorites.length == 0
-                        ?
-                        (
-                            <div className="no-recipes">
-                                <h2 className="title">Sorry, you haven't liked any recipes yet.</h2>
-                                {buttons}
-                            </div>
-                        )
-                        : <div className="loader"></div >
-            }
+            {myFavorites && myFavorites.length > 0 && myFavoriteRecipeCards}
+            {myFavorites && myFavorites.length === 0 && noRecipes}
+            {!myFavorites && <div className="loader"></div>}
         </div>
-    )
-
-    {/* <ng-container *ngIf="error">
-            <p className="error">
-                {{error}}
-            </p>
-        </ng-container> */}
+    );
 }
-
 
 export default MyFavorites;
--- client/src/components/Recipe/MyRecipes/MyRecipes.js
+++ client/src/components/Recipe/MyRecipes/MyRecipes.js
@@ -2,62 +2,54 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
 import * as contentService from '../../../services/contentService';
-import { useAuthContext } from '../../../contexts/AuthContext';
-
-import './MyRecipes.scss';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import './MyRecipes.scss';
 
 function MyRecipes() {
-    const { user } = useAuthContext();
     const navigate = useNavigate();
     const [error, setError] = useState(null);
     const [myRecipes, setMyRecipes] = useState({});
 
     useEffect(() => {
-        contentService.loadMyRecipes(user)
+        contentService.loadMyRecipes()
             .then(recipes => setMyRecipes(recipes))
             .catch(err => {
                 setError(err);
-                console.log(`Error: ${err}`);
+                console.error(err);
             });
     }, []);
 
-    const buttons = user.username
-        ? (
-            <div>
-                <p>Add one right away!</p>
-                <Link className="button" to="/recipes/new-recipe">Add New Recipe</Link>
-                <button className="button" onClick={() => navigate(-1)}>Go Back</button>
-            </div>
-        )
-        : <Link className="button" to="/">Go back home</Link>;
+    const buttons = (
+        <div>
+            <p>Add one right away!</p>
+            <Link className="button" to="/recipes/new-recipe">Add New Recipe</Link>
+            <button className="button cancel-btn" onClick={() => navigate(-1)}>Go Back</button>
+        </div>
+    );
+
+    const myRecipeCards = (
+        <div className="recipes">
+            <h2 className="title">My Recipes</h2>
+            {myRecipes.map(r => <RecipeCard key={r._id} recipe={r} />)}
+        </div>
+    );
+
+    const noRecipes = (
+        <div className="no-recipes">
+            <h2 className="title">Sorry, you have currently no recipes.</h2>
+            {buttons}
+        </div>
+    );
 
     return (
         <div className="MyRecipes">
             {error && <p className="error">{error.message}</p>}
 
-            {
-                myRecipes && myRecipes.length > 0
-                    ?
-                    (
-                        <div className="recipes">
-                            <h2 className="title">My Recipes</h2>
-                            {myRecipes.map(r => <RecipeCard key={r._id} recipe={r} />)}
-                        </div>
-                    )
-                    : myRecipes && myRecipes.length == 0
-                        ?
-                        (
-                            <div className="no-recipes">
-                                <h2 className="title">Sorry, you have currently no recipes.</h2>
-                                {buttons}
-                            </div>
-                        )
-                        : <div className="loader"></div >
-            }
-        </div>
-    )
+            {myRecipes && myRecipes.length > 0 && myRecipeCards}
+            {myRecipes && myRecipes.length == 0 && noRecipes}
+            {!myRecipes && <div className = "loader"></div>}
+        </div >
+    );
 }
-
 
 export default MyRecipes;
